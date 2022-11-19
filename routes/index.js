614573276574b6f6818bfeba91e95fe75321f61c
Jack Warrick <jackwarrick@Jacks-MBP.zyxel.com>
2022-11-19 13:52:32 -0600

    readme

--- routes/index.js
+++ routes/index.js
@@ -8,3 +8,4 @@ router.use((req, res) => res.send('Wrong route!'));
 module.exports = router;
 
 // Got this from the index.js next to the api folder in the mini project
+//Exports out for use by the server (index.js)
